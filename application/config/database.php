4bc5c68eefb57804f5a53f6664a4dbb52bfe14ad
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-11-19 15:47:17 +0530

    Change database configuration to Azure settings
    Updated database connection settings for Azure.

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'test.cbmouss2so7f.ap-south-1.rds.amazonaws.com',
-	'username' => 'admin',
-	'password' => 'admin123',
+	'hostname' => 'smithadb.mysql.database.azure.com',
+	'username' => 'smithadbuser',
+	'password' => 'Verity@123',
 	'database' => 'college_mgmt',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
